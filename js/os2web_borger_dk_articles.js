f45e6c897942e6c05f19e5590c802893a8126a35
Peter Sletten <peter@bellcom.dk>
2013-02-11 09:56:01 +0100

    first commit of module files

---
+++ js/os2web_borger_dk_articles.js
@@ -0,0 +1,17 @@
+(function($) {
+  Drupal.behaviors.os2web_borger_dk_articles = {
+    attach: function(context) {
+      $("div.mArticle").hide();
+      $(".microArticle h2.mArticle").click(function() {
+        var myid = $(this).attr('id');
+        var style = $('div.' + myid).css('display');
+        if (style == 'none') {
+          $("div." + myid).show("500");
+        }
+        else {
+          $("div." + myid).hide("500");
+        }
+      });
+    }
+  }
+})(jQuery);
