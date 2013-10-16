025b670ee4c2fb52cb829263f4bd47732502ae35
Mikhail Sokolovskiy <mikhail.sokolovskiy@propeople.com.ua>
2013-10-16 18:28:50 +0300

    Use class instead of inline styles

--- js/os2web_borger_dk_articles.js
+++ js/os2web_borger_dk_articles.js
@@ -4,19 +4,9 @@
       $("div.mArticle").hide();
       $(".microArticle h2.mArticle").click(function() {
         var myid = $(this).attr('id');
-        var style = $('div.' + myid).css('display');
-        if (style == 'none') {
-          $("div." + myid).show("500");
-          $(this).css({
-            'background-image' : 'url("../sites/all/modules/os2web_borger_dk_articles/images/foldIn.png")',
-          });
-        }
-        else {
-          $("div." + myid).hide("500");
-          $(this).css({
-            'background-image' : 'url("../sites/all/modules/os2web_borger_dk_articles/images/foldOut.png")',
-          });
-        }
+        $("div." + myid).toggle("500");
+        // Use class instead of background style, so theme can use it
+        $(this).toggleClass("expanded");
       });
     }
   }
